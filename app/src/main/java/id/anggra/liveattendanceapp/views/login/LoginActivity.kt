4718b72b8d5bc3c9fb4433fd99e25ee35614d0d1
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-03-08 20:16:49 +0700

    ui: add login page

---
+++ app/src/main/java/id/anggra/liveattendanceapp/views/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package id.anggra.liveattendanceapp.views.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.anggra.liveattendanceapp.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
