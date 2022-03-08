b97e3759d86c8fdb19722cab961663ee9d3c516f
anggraanutomo <anggraalheranasmitautomo@gmail.com>
2022-03-08 19:46:41 +0700

    ui: add splash screen

---
+++ app/src/main/java/id/anggra/liveattendanceapp/views/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package id.anggra.liveattendanceapp.views.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import id.anggra.liveattendanceapp.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
